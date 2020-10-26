e87a9565ebfcf44b8c7dd94899e9b81441cbe267
Fernando Chavez <fjchavez01@gmail.com>
2020-10-26 12:54:01 -0700

    added functions to handel all changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
+const items = [{
+  text: 'text',
+  id: 123,
+  crossed: false,
+},]
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(){
+    super()
+    this.state={
+      items: items
+    }
+  }
+  
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>Ferns Todo App!</h2>
       </div>
     );
   }
